50a94f55514c5561f153a49d13cb54cd91160a64
copiousamounts <nick.hernstedt@gmail.com>
2020-04-30 10:33:28 -0700

    finished controller.js page

---
+++ controllers/controller.js
@@ -0,0 +1,21 @@
+var express = require("express");
+const path = require("path");
+var router = express.Router();
+
+//  path to main page
+router.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/index.html`));
+});
+
+//  path to exercise page
+router.get(`/exercise?`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/exercise.html`));
+});
+
+// path to see your stats
+router.get(`/stats`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/stats.html`));
+});
+
+// exporting the controller
+module.exports = router;
